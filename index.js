f6a151dc497468250b1ca179cff0284cf7e126cc
Jddg95 <140569072+Jddg95@users.noreply.github.com>
2023-10-19 23:13:56 -0500

    created questions object

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const questions 
+const xmlbuilder = require('xmlbuilder');
+
+// Defining the questions for the user input 
+const logoQuestions = [
+    {
+        type: 'input',
+        name: 'text',
+        messages: 'Enter up tp three characters for the text:',
+    },
+    {
+        type:'input',
+        name: 'textColor',
+        message: 'Enter text color (keyword or hex #',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        chouces: ['circle', 'triangle', 'square'],
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter shape color (keyword or hex):',
+    }
+];
 
 
 
