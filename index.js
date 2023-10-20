abb4b6f443b844cdd92b2be39c2e25f0c23d4fda
Jddg95 <140569072+Jddg95@users.noreply.github.com>
2023-10-19 23:42:23 -0500

    dd code to generate and save an SVG logo

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 const xmlbuilder = require('xmlbuilder');
 
 // Defining the questions for the user input 
-const logoQuestions = [
+const questions = [
     {
         type: 'input',
         name: 'text',
-        messages: 'Enter up tp three characters for the text:',
+        message: 'Enter up tp three characters for the text:',
     },
     {
         type:'input',
@@ -28,9 +28,31 @@ const logoQuestions = [
 ];
 
 // Inquierer to prompt the user for input
-inquierer.prompt(logoQuestions).then((answers) => {
+inquierer.prompt(questions).then((answers) => {
     const {text, textColor, shape, shapeColor} = answers;
-}) 
+
+    const svg = xmlbuilder.create('svg');
+    svg.att('width', '300');
+    svg.att('height', '200');
+
+    //create shape element 
+    const shapeElement = svg.ele(shape);
+    shapeElement.att(fill, shapeColor);
+
+    // text for the sahape 
+    const textElement = shapeElement.ele('text', {x: '50%', y: '50%', 'text-anchor': 'middle', dy: '0.35em'});
+    textElement.att('fill', textColor);
+    textElement.text(text);
+
+    // convert svg content to string 
+    const svgString = svg.end({ pretty: true });
+
+    //writting the svg content to a file named "logo.svg"
+    fs.writeFileSync('logo.svg', svgString);
+
+    //Print the generated logo to the command line
+    console.log(svgString);
+}); 
 
 
 
